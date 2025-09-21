d9583108d2e6af21646e9a90fc56904672b268a9
DESKTOP-IA7BFTA\hp <mohamedmedo9932@gmail.com>
2025-09-21 10:20:20 +0300

    - CupertinoNavigationBar

--- lib/cupertino_widgets/cupertino_page_scaffold.dart
+++ lib/cupertino_widgets/cupertino_page_scaffold.dart
@@ -6,11 +6,27 @@ class TestScreen extends StatelessWidget {
 
   @override
   // Cupertino widgets provide iOS-style UI components for Flutter apps
-  // CupertinoPageScaffold it's like Scaffold widget
+  // CupertinoNavigationBar it's like Appbar widget
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      backgroundColor: Colors.redAccent,
-      navigationBar: CupertinoNavigationBar(),
+      backgroundColor: Colors.deepPurpleAccent,
+      navigationBar: CupertinoNavigationBar(
+        //Whether the navigation bar appears transparent when no content is scrolled under.
+        //If you choose false you can see your app bar
+        automaticBackgroundVisibility: false,
+        // like title in the app bar widget
+        middle: Text('Testing'),
+        backgroundColor: Colors.black45,
+        // you can choose here the brightness for the notificatin bar color
+        brightness: Brightness.dark,
+
+        //Manually specify the previous route's title when automatically implying the leading back button.
+        previousPageTitle: 'String',
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+        ),
+      ),
       child: Column(children: [Container()]),
     );
   }
