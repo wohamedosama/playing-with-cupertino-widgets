b6f939f110f0cef26523a682576434c32a9798ea
DESKTOP-IA7BFTA\hp <mohamedmedo9932@gmail.com>
2025-09-21 11:12:52 +0300

    -Cupertino Activity Indicator

--- lib/cupertino_widgets/cupertino_page_scaffold.dart
+++ lib/cupertino_widgets/cupertino_page_scaffold.dart
@@ -15,43 +15,7 @@ class TestScreen extends StatelessWidget {
         brightness: Brightness.dark,
       ),
       child: Center(
-        child: CupertinoButton(
-          child: Text(
-            'Cupertino Action Sheet',
-            style: TextStyle(color: Colors.white),
-          ),
-          onPressed: () {
-            showCupertinoSheet(
-              context: context,
-              builder: (context) {
-                return CupertinoActionSheet(
-                  actionScrollController: ScrollController(
-                    initialScrollOffset: 0.5,
-                  ),
-                  message: Text('Action Sheet'),
-                  actions: <CupertinoActionSheetAction>[
-                    CupertinoActionSheetAction(
-                      isDefaultAction: true,
-                      child: Text(
-                        'Pick Image from the gallery',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                    ),
-                    CupertinoActionSheetAction(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: Text('Cancel'),
-                    ),
-                  ],
-                );
-              },
-            );
-          },
-        ),
+        child: CupertinoActivityIndicator(color: Colors.yellow, radius: 20),
       ),
     );
   }
