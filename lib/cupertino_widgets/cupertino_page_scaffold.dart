8449e190281202e729a80ed55760198e12a36315
DESKTOP-IA7BFTA\hp <mohamedmedo9932@gmail.com>
2025-09-21 12:06:33 +0300

    - Cupertino Alert Dialog

--- lib/cupertino_widgets/cupertino_page_scaffold.dart
+++ lib/cupertino_widgets/cupertino_page_scaffold.dart
@@ -15,7 +15,37 @@ class TestScreen extends StatelessWidget {
         brightness: Brightness.dark,
       ),
       child: Center(
-        child: CupertinoActivityIndicator(color: Colors.yellow, radius: 20),
+        child: CupertinoButton(
+          child: Text(
+            'Alert Dialog ⚠️',
+            style: TextStyle(color: Colors.white, fontSize: 22),
+          ),
+          onPressed: () {
+            showCupertinoDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return CupertinoAlertDialog(
+                  title: Text('Alert ⚠️'),
+                  content: Text('Flutter Mentorship Round3'),
+                  actions: [
+                    CupertinoDialogAction(
+                      child: Text('No'),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                    CupertinoDialogAction(
+                      child: Text('Yes', style: TextStyle(color: Colors.white)),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
