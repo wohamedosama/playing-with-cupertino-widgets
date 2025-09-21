219a192a7ea86b26989ca97fd4b301bf19e0d1cc
DESKTOP-IA7BFTA\hp <mohamedmedo9932@gmail.com>
2025-09-21 09:41:18 +0300

    playing with cupertino widgets

---
+++ android/app/src/main/kotlin/com/example/cupertino_widgets_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cupertino_widgets_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:cupertino_widgets_test/cupertino_widgets/test_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      title: 'Testing Applciation ',
+      home: TestScreen(),
+      debugShowCheckedModeBanner: false,
+      theme: CupertinoThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.red,
+      ),
+    );
+  }
+}
